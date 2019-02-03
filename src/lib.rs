f240c74259436fe437e4d00de2bc339619cc9143
ltoddy <taoliu0509@gmail.com>
2019-02-03 23:35:15 +0800

    WIP: add color module base elements

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,14 @@
+mod color;
 pub mod random;
+
+pub struct Provider {
+    pub color: color::Provider,
+}
+
+impl Provider {
+    pub fn new() -> Self {
+        Provider {
+            color: color::Provider::new(),
+        }
+    }
+}
